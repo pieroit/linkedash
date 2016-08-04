ee30b9d8beebad62e73728da5ad7732dd43190af
pieroit <piero.savastano@gmail.com>
2016-08-04 21:47:47 +0200

    add parallel SPARQL queries with promises; add leaflet with poligon

--- js/main.js
+++ js/main.js
@@ -12,10 +12,15 @@ jQuery(document).ready(function(){
     
     $('#lookup-run').click(runLookupQuery);
     
-    smartClient.fragmentsClient = new ldf.FragmentsClient('http://fragments.dbpedia.org/2015/en');
+    smartClient.fragments = {
+        'dbpedia' : new ldf.FragmentsClient('http://fragments.dbpedia.org/2015/en'),
+        'ispra'   : new ldf.FragmentsClient('http://localhost:5000/ispra')
+    }
 });
 
 function runLookupQuery(){
+    
+    $('#results').empty();
     
     var query = $('#lookup-query').val();
     var dbPediaLookupURI = 'http://lookup.dbpedia.org/api/search.asmx/KeywordSearch?QueryString=' + query;
@@ -25,49 +30,61 @@ function runLookupQuery(){
         if(_.has(lookupResults, 'results')){
             // TODO: disambiguation
             
+            smartClient.graph = [];
             var mainURI = lookupResults['results'][0]['uri'];
-            runMainSparqlQuery(mainURI);
-            runISPRASparqlQuery(mainURI);
+            
+            var sparqlPromises = [runMainSparqlQuery(mainURI), runISPRASparqlQuery(mainURI)];
+            Promise.all(sparqlPromises).then(function(result){
+                //console.log(smartClient.graph);
+                runTemplate(mainURI);
+            }).catch(function(err){
+                console.error(err);
+            });
+            
+            //runISPRASparqlQuery(mainURI);
         }
     });
 }
 
 function runISPRASparqlQuery(uri){
     
-    /*
-     * SELECT * WHERE {?s <http://www.w3.org/2002/07/owl#sameAs> <http://dbpedia.org/resource/Guidonia_Montecelio> . } LIMIT 1000
-     */
-    
-    console.warn('no LDF for ISPRA');
+    return new Promise(function(resolve, reject){
+        var query = 'CONSTRUCT WHERE { ' +
+            '?s <http://www.w3.org/2002/07/owl#sameAs> <' + uri + '> . ' +
+            '?s ?p ?o . ' +
+        '} LIMIT 1000';
+
+        var results = new ldf.SparqlIterator(query, { fragmentsClient: smartClient.fragments['ispra'] });
+        
+        results.on('data', function(d){
+            smartClient.graph.push(d);
+        });
+        results.on('end', function(){
+            resolve();
+        });
+    });
+
 }
 
 function runMainSparqlQuery(uri){
     
-    $('#results').empty();
-    /* TODO: find other names
-    var query = 'SELECT * WHERE {' +
-        '?s <http://www.w3.org/2002/07/owl#sameAs> <' + uri + '> . ' +
-    '} LIMIT 1000';*/
-    var query = 'CONSTRUCT WHERE {' +
-        '<' + uri + '> ?p ?o . ' +
-    '} LIMIT 1000';
-    
-    smartClient.graph = [];
-    var results = new ldf.SparqlIterator(query, { fragmentsClient: smartClient.fragmentsClient });
-    results.on('data', function(d){
-        smartClient.graph.push(d);
-        $('#results').append('<p>' + stringifyTriple(d) + '</p>' );
-    });
-    results.on('end', function(){
-        runTemplate(uri);
+    return new Promise(function(resolve, reject){
+
+        var query = 'CONSTRUCT WHERE {' +
+            '<' + uri + '> ?p ?o . ' +
+        '} LIMIT 1000';
+
+        var results = new ldf.SparqlIterator(query, { fragmentsClient: smartClient.fragments['dbpedia'] });
+        results.on('data', function(d){
+            smartClient.graph.push(d);
+        });
+        results.on('end', function(){
+            resolve();
+        });
     });
 }
 
 function runTemplate(uri){
-    
-    _.each(smartClient.graph, function(t){
-        //console.log(t);
-    });
     
     var templateData = {
         title       : findFirstInGraph(uri, 'http://www.w3.org/2000/01/rdf-schema#label'),
@@ -79,6 +96,38 @@ function runTemplate(uri){
     var template = Handlebars.compile(source);
     
     $('#results').append( template(templateData) );
+    
+    function toNumber(s){
+        return +s.replace(/"/g, '');
+    }
+    
+    var alternateURI = 'http://dati.isprambiente.it/id/place/58047';    // TODO: this should be automagic
+    var centerLat = findFirstInGraph(alternateURI, 'http://www.w3.org/2003/01/geo/wgs84_pos#lat');
+    centerLat = toNumber(centerLat);
+    var centerLon = findFirstInGraph(alternateURI, 'http://www.w3.org/2003/01/geo/wgs84_pos#long');
+    centerLon = toNumber(centerLon);
+    var polygon   = findFirstInGraph(alternateURI, 'http://www.opengis.net/ont/gml#polygon');
+    polygon = polygon.replace(/"/g, '').split(', ');
+    polygon = _.map(polygon, function(p){
+        p = p.split(' ');
+        return [ toNumber(p[1]), toNumber(p[0]) ];
+    });
+    
+    console.log(centerLat, centerLon, polygon);
+    
+    var geoJSON   = {
+        "type": "Feature",
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [polygon]
+        }
+    };
+    console.log(centerLat, centerLon, polygon);
+    var map = L.map('map-container').setView( [ centerLat, centerLon ], 10);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    L.geoJson(geoJSON).addTo(map);
 }
 
 function findFirstInGraph(subject, predicate){
@@ -86,7 +135,6 @@ function findFirstInGraph(subject, predicate){
     var triple = _.find(smartClient.graph, function(t){
         return (t.subject == subject) && (t.predicate == predicate);
     });
-    
     return triple.object;
 }
 
